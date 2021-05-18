bd04b8037a8764ecb01adba5f41da452fa8a5dea
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-18 09:56:51 +0530

    Add project files.

---
+++ IndianStateCensusAnlyser/CSVAdapterFactory.cs
@@ -0,0 +1,23 @@
+﻿using IndianStateCensusAnlyzer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensusAnlyzer
+{
+    class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                //case (CensusAnalyser.Country.US):
+                //    return new USCensusAdapter().LoadUSCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
